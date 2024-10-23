287ec8c64b7862411d3672df4a3ca036290e49e1
Vladimir Tinica <vltinica@gmail.com>
2024-10-22 20:51:54 -0400

    some styles

---
+++ src/components/Todos/Todo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Todo.module.css"
+
+const Todo = ({ todo }) => {
+  return (
+    <div className={styles.todo}>
+      <div className={styles.todoText}>{todo}</div>
+    </div>
+  );
+};
+
+export default Todo;
---
+++ src/components/Todos/TodoList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Todo from "./Todo";
+
+import styles from "./TodoList.module.css"
+
+const TodoList = ({todos}) => {
+  return (
+    <div className={styles.todoListContainer }>
+      {todos.map((todo, index) => <Todo key={index} todo={todo}/>)}
+    </div>
+  );
+};
+
+export default TodoList;
