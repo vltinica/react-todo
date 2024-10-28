ca6de6cc5c1f7e9e411cc038368ef94150d5b708
Vladimir Tinica <vltinica@gmail.com>
2024-10-27 20:26:03 -0400

    unique id

--- src/components/Todos/Todo.jsx
+++ src/components/Todos/Todo.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { RiTodoFill } from "react-icons/ri";
 import styles from "./Todo.module.css"
 
-const Todo = ({ todo, index, deleteTodo }) => {
+const Todo = ({ todo, deleteTodo }) => {
   return (
-    <div className={styles.todo} onDoubleClick={() =>deleteTodo(index)}>
+    <div className={styles.todo} onDoubleClick={() => deleteTodo(todo.id)}>
       <RiTodoFill className={styles.todoIcon }/>
-      <div className={styles.todoText}>{todo}</div>
+      <div className={styles.todoText}>{todo.text}</div>
     </div>
   );
 };
--- src/components/Todos/TodoList.jsx
+++ src/components/Todos/TodoList.jsx
@@ -7,7 +7,7 @@ const TodoList = ({todos, deleteTodo}) => {
   return (
     <div className={styles.todoListContainer}>
       {!todos.length && <h2>Todo list is empty</h2>}
-      {todos.map((todo, index) => <Todo key={index} todo={todo} index={index}   deleteTodo={deleteTodo}/>)}
+      {todos.map((todo) => <Todo key={todo.id} todo={todo}   deleteTodo={deleteTodo}/>)}
     </div>
   );
 };
